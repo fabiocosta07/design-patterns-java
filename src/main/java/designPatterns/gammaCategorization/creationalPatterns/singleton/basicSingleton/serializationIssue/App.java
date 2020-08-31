35ca53934b1c02d9fd2282e7d824d19590746164
Fabio Apolonio <fabioapolonio@brq.com>
2020-08-31 16:01:53 -0300

    singleton example

---
+++ src/main/java/designPatterns/gammaCategorization/creationalPatterns/singleton/basicSingleton/serializationIssue/App.java
@@ -0,0 +1,34 @@
+package designPatterns.gammaCategorization.creationalPatterns.singleton.basicSingleton.serializationIssue;
+
+public class App {
+    public static void main(String[] args) {
+        BasicSingleton singleton = BasicSingleton.getInstance();
+        singleton.setValue(222);
+        BasicSingleton singleton1 = BasicSingleton.getInstance();
+        singleton1.setValue(333);
+        System.out.println(singleton.getValue());
+    }
+}
+
+class BasicSingleton {
+    private static BasicSingleton INSTANCE;
+    private int value;
+
+    private BasicSingleton() {
+    }
+
+    public static BasicSingleton getInstance() {
+        if (INSTANCE == null){
+            INSTANCE = new BasicSingleton();
+        }
+        return INSTANCE;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
